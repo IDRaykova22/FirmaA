be61f36194785bc828d25743e80da1c69af74dac
KDIliev22 <KDIliev22@codingburgas.bg>
2026-09-21 11:53:09 +0300

    Initial project setup

---
+++ backend/src/main/java/com/firmaa/techdept/TechdeptApplication.java
@@ -0,0 +1,13 @@
+package com.firmaa.techdept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechdeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechdeptApplication.class, args);
+	}
+
+}
